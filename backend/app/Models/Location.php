c0ad6568f1adae6bbf6a16134d414b1d74bf4d24
Juan Jaramillo <ingenierojaramillo@gmaial.com>
2025-08-08 23:53:39 -0500

    build(phpstan): fix larastan include for Laravel 12 and silence HasFactory generic

--- backend/app/Models/Location.php
+++ backend/app/Models/Location.php
@@ -5,9 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @phpstan-use \Illuminate\Database\Eloquent\Factories\HasFactory<\Database\Factories\LocationFactory>
+ * @method static \Database\Factories\LocationFactory factory($count = null, $state = [])
+ */
 class Location extends Model
 {
     use HasFactory;
 
     protected $fillable = ['code', 'name', 'image'];
+
+    protected static function newFactory(): \Database\Factories\LocationFactory
+    {
+        return \Database\Factories\LocationFactory::new();
+    }
 }
